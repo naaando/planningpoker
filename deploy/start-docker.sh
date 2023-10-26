6e0de866e8c011191cccc663a14c2e15a0154670
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-10-26 00:20:53 -0300

    Atualiza script de inicializar docker

--- deploy/start-docker.sh
+++ deploy/start-docker.sh
@@ -1,2 +1,2 @@
 docker pull ghcr.io/naaando/planningpoker:master
-docker run -dp 9001:80 --env-file .env ghcr.io/naaando/planningpoker:master
+docker run --name=planningpoker -d -p 9001:80 --env-file .env --add-host=host.docker.internal:host-gateway ghcr.io/naaando/planningpoker:master
