b6cbdfff836cb096ad055f4670a80d6693ad26cb
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-10-28 00:18:33 -0300

    Adiciona salas

---
+++ database/migrations/2023_10_27_025852_create_rooms_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->string('slug')->primary();
+            $table->string('name');
+            $table->foreignUlid('round_id')->nullable()->constrained('rounds')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
