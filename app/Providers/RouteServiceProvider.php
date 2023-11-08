f1d25d807207c4dc7962c44a8b1b2fada13c16a4
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-11-08 12:17:26 -0300

    Adiciona rate limit global

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(1000);
+        });
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
