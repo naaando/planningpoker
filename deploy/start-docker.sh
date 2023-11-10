ecd660329e13d5fe68569629a14036d070d62564
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-11-10 10:26:29 -0300

    Adiciona flag restart always

--- deploy/start-docker.sh
+++ deploy/start-docker.sh
@@ -1,3 +1,3 @@
 docker pull ghcr.io/naaando/planningpoker:master
 docker rm -f planningpoker
-docker run --name=planningpoker -d -p 9001:80 --env-file .env --add-host=host.docker.internal:host-gateway ghcr.io/naaando/planningpoker:master --restart=always
+docker run --name=planningpoker -d -p 9001:80 --restart=always --env-file .env --add-host=host.docker.internal:host-gateway ghcr.io/naaando/planningpoker:master
